18be23f46d31855906d5e4ea0907434fdb3b7893
Giada Sponzilli <sponzilli.g@gmail.com>
2024-01-09 18:08:27 +0000

    added function for time color blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,12 +37,13 @@ function createTimeblocks() {
     const container = $(`.container`)
 
     for (let i = 9; i <= 17; i++) {
-        const hour = dayjs().hour(i).format(`H`)
+        const timeBlockHour = dayjs().hour(i).format(`H`)
         const timeBlock = $(`<div class= "time-block row" data-i= "${i}">
-            <div class = "col-2 hour">${hour}</div>
+            <div class = "col-2 hour">${timeBlockHour}</div>
             <textarea class = "col-8 description"></textarea>
             <button class = "col-2 saveBtn"><i class="fa-solid fa-floppy-disk"></i></i></button>
         </div>`)
+        
 
         container.append(timeBlock)
     }
@@ -53,3 +54,27 @@ function createTimeblocks() {
 createTimeblocks()
 
 
+
+// Color-code each time block based on past, present, and future when the time block is viewed.
+
+function colorTime() {
+    const textAreaTimeColor= $(`.description`)
+    const currentHour = dayjs().hour();
+
+    const hourStoredInTimeBlocks = $(this).data(`i`);
+
+    $.each(textAreaTimeColor, function() {
+        if (hourStoredInTimeBlocks < currentHour) {
+        textAreaTimeColor.addClass(`past`)
+        }  else if (hourStoredInTimeBlocks === currentHour) {
+        textAreaTimeColor.addClass(`present`)
+        }  else if (hourStoredInTimeBlocks > currentHour) {
+        textAreaTimeColor.addClass(`future`)
+        }
+
+        })
+        
+    
+}
+
+colorTime()
